450857281a447934a1baeb04f8a946adf22663dd
Najmul shaon <najmul.nh.shaon@gmail.com>
2023-12-30 15:09:05 +0600

    hashtags added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,7 +1,47 @@
 import PropTypes from "prop-types";
 const Blog = ({ blog }) => {
-  console.log(blog);
-  return <div></div>;
+  //   console.log(blog);
+  const {
+    id,
+    title,
+    img,
+    author_name,
+    author_img,
+    post_date,
+    reading_time,
+    hashtags,
+  } = blog;
+  console.log(hashtags);
+  return (
+    <div>
+      <img className="w-full" src={img} alt="" />
+      <div className="flex justify-between items-center">
+        <div className="flex items-center">
+          <div>
+            <img className="w-14 rounded-full" src={author_img} alt="" />
+          </div>
+          <div className="mx-6">
+            <h3 className="text-black text-2xl font-bold">{author_name}</h3>
+            <p>ID: {id}</p>
+            <p className="text-sm font-semibold text-black/60">{post_date}</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-sm font-medium text-black/60">
+            {reading_time} min to read
+          </p>
+        </div>
+      </div>
+      <h3 className="text-black text-4xl font-bold">{title}</h3>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span className="mr-4" key={idx}>
+            <a href="">{hash}</a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -12,6 +13,9 @@ const Blogs = () => {
   return (
     <div className="border-2 border-orange-500 border-solid md:w-2/3">
       <h3 className="text-3xl">Blogs: {blogs.length}</h3>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog}></Blog>
+      ))}
     </div>
   );
 };
